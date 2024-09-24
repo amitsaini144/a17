0a47c42fee69526b0da579d4ff5b468845a34729
amitsaini144 <1amit24saini5@gmail.com>
2024-09-24 20:34:19 +0530

    image change

--- src/components/homepage/CategorySection.tsx
+++ src/components/homepage/CategorySection.tsx
@@ -24,12 +24,12 @@ export default function CategorySection() {
                             </div>
                         </div>
                         <div className="bg-[#f7f7f7] rounded-b-3xl">
-                            <Image src="/ultrawatch.png" alt="Headphones x-28m" width={600} height={360} quality={90} priority />
+                            <Image src="/ultrawatch.png" alt="Headphones x-28m" width={612} height={360} quality={90} priority />
                         </div>
                     </div>
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-8">
                         <div className="flex">
-                            <div className="flex flex-col gap-20 bg-[#f7f7f7] p-10 pr-0 rounded-l-3xl">
+                            <div className="flex flex-col justify-between bg-[#f7f7f7] p-10 pr-0 rounded-l-3xl">
                                 <div className="flex flex-col gap-2">
                                     <h2 className="text-2xl text-black font-medium">Displays</h2>
                                     <p className="text-base">Experience crystal-clear clarity and vibrant visuals with our Displays.</p>
@@ -41,15 +41,15 @@ export default function CategorySection() {
                                 </div>
                             </div>
                             <div className="bg-[#f7f7f7] rounded-r-3xl">
-                                <Image src="/eDisplay.png" alt="monitor" width={450} height={200} quality={90} priority />
+                                <Image src="/macbook.png" alt="monitor" width={411} height={300} quality={90} priority className="mix-blend-darken" />
                             </div>
                         </div>
                         <div className="flex gap-6">
                             <div className="bg-[#f7f7f7] rounded-3xl overflow-hidden">
-                                <Image src="/whiteHeadphone.png" alt="headphone" width={400} height={600} quality={90} priority className="mix-blend-multiply" />
+                                <Image src="/whiteHeadphone.png" alt="headphone" width={294} height={600} quality={90} priority className="mix-blend-multiply" />
                             </div>
                             <div className="bg-[#f7f7f7] rounded-3xl overflow-hidden">
-                                <Image src="/iphonepro.png" alt="phone" width={400} height={600} quality={90} priority className="mix-blend-multiply" />
+                                <Image src="/iphonepro.png" alt="phone" width={294} height={600} quality={90} priority className="mix-blend-multiply" />
                             </div>
                         </div>
                     </div>
