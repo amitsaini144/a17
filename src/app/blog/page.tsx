bd4eb08a7f1aaf9957e80dee3e5859042f950a5f
amitsaini144 <1amit24saini5@gmail.com>
2024-09-30 06:08:49 +0530

    blog page complete

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,30 @@
+import BlogCard from "@/components/faq/BlogCard"
+
+const articles = [
+    { id: 1, label: 'The Future of Wearable Tech: Trends and Innovations to Watch', href: '/', image: '/VR.png', },
+    { id: 2, label: 'The Rise of Smart Home Devices: Transforming the Way We Live', href: '/', image: '/pc.png', },
+    { id: 3, label: 'Gaming Gadgets: Revolutionizing Entertainment and Beyond', href: '/', image: '/typing.png', },
+]
 
 export default function Blog() {
     return (
-        <div></div>
+        <div className="flex flex-col items-center w-full min-w-[320px] bg-white">
+            <div className="w-full max-w-[1349px]">
+                <div className="w-full flex flex-col gap-12 px-4 md:px-6 xl:px-10 pt-32 md:pt-40">
+                    <div className="flex flex-col gap-6">
+                        <h1 className="text-6xl text-black font-semibold">Blog</h1>
+                        <p className="text-xl text-[#4a4a4a]">Stay informed with our bite-sized articles</p>
+                    </div>
+
+                    <div className="flex flex-wrap justify-center items-center gap-6">
+                        {articles.map((article) => (
+                            <div className="" key={article.id}>
+                                <BlogCard article={article} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
---
+++ src/components/faq/BlogCard.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+
+interface ArticlesProps {
+    article: {
+        id: number;
+        label: string;
+        href: string;
+        image: string;
+    };
+}
+
+export default function BlogCard({ article }: ArticlesProps) {
+    return (
+        <motion.div whileHover={{ scale: 1.02 }} className="w-full">
+            <Link href="/" className="flex flex-col gap-4 w-full xl:w-[400px]">
+                <div className="rounded-3xl w-full">
+                    <Image
+                        src={article.image}
+                        alt="Ariticle image"
+                        width={400}
+                        height={400}
+                        quality={90}
+                        priority
+                        className="rounded-3xl object-cover h-96 w-full xl:w-[400px]" />
+                </div>
+                <div className="flex">
+                    <p className="text-black text-lg md:text-xl">{article.label}</p>
+                </div>
+            </Link>
+        </motion.div>
+    )
+}
