7563fe9ca7762ba368553369e09f6c73b068f1b6
amitsaini144 <1amit24saini5@gmail.com>
2024-10-02 02:36:50 +0530

    contact page complete

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,77 @@
+"use client"
+import { useState } from "react"
+
 export default function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        // Handle form submission logic here
+        console.log('Form submitted:', { name, email, message })
+    }
+
     return (
-        <div></div>
+        <div className="flex flex-col items-center w-full min-w-[320px] bg-white">
+            <div className="w-full max-w-[1349px]">
+                <div className="w-full flex flex-col gap-12 px-4 md:px-10 pt-32 md:pt-40">
+                    <div className="flex flex-col gap-6">
+                        <h1 className="text-6xl text-black font-semibold">Contact us</h1>
+                        <p className="text-lg md:text-xl text-black md:w-2/3">We&apos;re here to assist you with any inquiries, feedback, or assistance you may need. Whether you have questions about products, orders, or general inquiries, our dedicated customer support team is ready to help.</p>
+                    </div>
+                </div>
+                <div className="flex flex-col-reverse xl:flex-row px-4 md:px-10 pt-6 md:pt-10 gap-6">
+                    <div className="flex flex-col gap-6 xl:w-1/3 justify-between">
+                        <div className="flex flex-col justify-between px-4 py-6 md:p-6 bg-[#f7f7f7] rounded-3xl h-[250px] xl:h-1/2">
+                            <div className="flex flex-col gap-2 text-lg md:text-xl text-black">
+                                <p>Phone number</p>
+                                <p>+ 123 456 789</p>
+                            </div>
+                            <p className="text-base md:text-lg text-[#4a4a4a]">Call our team directly. We will provide information, answer technical questions, or schedule an appointment.</p>
+                        </div>
+                        <div className="flex flex-col justify-between px-4 py-6 md:p-6 bg-[#f7f7f7] rounded-3xl h-[250px] xl:h-1/2">
+                            <div className="flex flex-col gap-2 text-lg md:text-xl text-black">
+                                <p>E-mail</p>
+                                <p>example@gmail.com</p>
+                            </div>
+                            <p className="text-base md:text-lg text-[#4a4a4a]">Use email to provide information about the project. We will respond as soon as possible, providing comprehensive support.</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col px-4 py-6 md:p-6 bg-[#f7f7f7] rounded-3xl xl:w-2/3 gap-6">
+                        <div className="flex flex-col gap-2">
+                            <p className="text-lg md:text-xl text-black">Drop us a message</p>
+                            <p className="md:w-2/3 text-[#4a4a4a] text-base md:text-lg">We&apos;re always here to assist you at Etec. Our customer support team is dedicated to addressing your inquiries.</p>
+                        </div>
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                            <div className="flex flex-col md:flex-row w-full gap-3">
+                                <input
+                                    required
+                                    type="text"
+                                    value={name}
+                                    placeholder="Name"
+
+                                    onChange={(e) => setName(e.target.value)}
+                                    className="p-4 rounded-full w-full focus:outline-none" />
+                                <input
+                                    required
+                                    type="email"
+                                    value={email}
+                                    placeholder="Email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="p-4 rounded-3xl w-full focus:outline-none" />
+                            </div>
+                            <textarea
+                                required
+                                value={message}
+                                placeholder="Message"
+                                onChange={(e) => setMessage(e.target.value)}
+                                className="p-4 w-full h-80 resize-none rounded-3xl focus:outline-none"></textarea>
+                            <input type="submit" value="Send message" className="p-4 rounded-3xl w-full bg-black text-white cursor-pointer" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
