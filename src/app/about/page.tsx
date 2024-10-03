dea9d1984e924222c0f3b2e067f1f9fc80540e0a
amitsaini144 <1amit24saini5@gmail.com>
2024-10-04 00:11:56 +0530

    about page complete

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { TruckIcon, CreditCardIcon, Undo2Icon } from "lucide-react"
 import SmallCard from "@/components/about/SmallCard"
 import LargeCard from "@/components/about/LargeCard"
+import Link from "next/link"
 
 const smallCardInfo = [
     { id: 1, svg: TruckIcon, title: 'Free Shipping', description: '$24+ orders ship free' },
@@ -40,6 +41,30 @@ export default function About() {
                         {largeCardInfo.map((card) => (
                             <LargeCard key={card.id} card={card} />
                         ))}
+                    </div>
+                </div>
+                <div className="flex px-4 pb-10 pt-0 md:px-6 md:py-10 xl:p-10">
+                    <div className="flex flex-col md:flex-row w-full gap-6 md:gap-16 items-center">
+                        <Image src="/premium.png" alt="about" width={760} height={500} quality={90} priority className="object-cover w-full md:w-1/2 h-[390px] rounded-3xl " />
+                        <div className="flex flex-col gap-6 text-center items-center justify-center h-fit md:w-1/2">
+                            <h3 className="text-2xl md:text-[28px] xl:text-[32px] text-black">Premium Tech Selection</h3>
+                            <p className="md:text-lg text-[#4a4a4a]">Our pursuit of excellence involves extensive exploration of top-tier factories globally, ensuring our tech lineup mirrors the standards upheld by renowned brands. We forge strong partnerships with these facilities, prioritizing factors like sustainability and ethical production practices.</p>
+                            <Link href='/shop' className="border border-black px-8 py-4 rounded-full md:text-lg w-fit">
+                                Check our store
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex px-4 pb-0 pt-6 md:px-6 md:py-10 xl:p-10 xl:pb-0">
+                    <div className="flex flex-col md:flex-row-reverse w-full gap-6 md:gap-16 items-center">
+                        <Image src="/timeless.png" alt="about" width={760} height={500} quality={90} priority className="object-cover w-full md:w-1/2 h-[390px] rounded-3xl " />
+                        <div className="flex flex-col gap-6 text-center items-center justify-center h-fit md:w-1/2">
+                            <h3 className="text-2xl md:text-[28px] xl:text-[32px] text-black">Timeless Tech Solutions</h3>
+                            <p className="md:text-lg text-[#4a4a4a]">At A17, we eschew passing trends in favor of enduring value and utility. Our commitment? To offer tech solutions that seamlessly integrate into your life, promising years of reliable performance and timeless appeal.</p>
+                            <Link href='/blog' className="border border-black px-8 py-4 rounded-full md:text-lg w-fit">
+                                Check our blog
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
