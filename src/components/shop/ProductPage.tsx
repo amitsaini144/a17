228c63ccb73a813bce1dff5de3ed2e34728d32c7
amitsaini144 <1amit24saini5@gmail.com>
2024-10-13 03:34:46 +0530

    layout update

--- src/components/shop/ProductPage.tsx
+++ src/components/shop/ProductPage.tsx
@@ -16,39 +16,71 @@ export default function ProductPage({ product }: { product: product }) {
                         <DotIcon className="w-4 h-4 text-[#bebebe]" />
                         <span className="text-black">{product.label}</span>
                     </div>
-                    <div className="flex gap-12 w-full">
-                        <div className="flex gap-4 ">
-                            <div className="flex flex-col gap-4">
-                                <Image src={product.image} alt={product.label} width={180} height={400} quality={90} priority placeholder="blur" className="rounded-3xl object-cover h-1/3" />
-                                <Image src={product.image} alt={product.label} width={180} height={400} quality={90} priority placeholder="blur" className="rounded-3xl object-cover h-1/2" />
-                                <Image src={product.image} alt={product.label} width={180} height={400} quality={90} priority placeholder="blur" className="rounded-3xl object-cover h-1/3" />
+                    <div className="flex flex-col xl:flex-row gap-10 w-full">
+                        <div className="flex flex-col-reverse md:flex-row gap-4">
+                            <div className="flex md:flex-col justify-between md:justify-around gap-2 md:gap-3 md:flex-shrink-0">
+                                <Image
+                                    src={product.image}
+                                    alt={product.label}
+                                    width={180}
+                                    height={180}
+                                    quality={90}
+                                    priority
+                                    placeholder="blur"
+                                    className="rounded-2xl object-cover min-w-[145px] w-1/3 h-[120px] md:w-[200px] md:h-[240px] xl:h-[180px]" />
+                                <Image
+                                    src={product.image}
+                                    alt={product.label}
+                                    width={180}
+                                    height={240}
+                                    quality={90}
+                                    priority
+                                    placeholder="blur"
+                                    className="rounded-2xl object-cover min-w-[145px] w-1/3 h-[120px] md:w-[200px] md:h-[240px] xl:h-[180px]" />
+                                <Image
+                                    src={product.image}
+                                    alt={product.label}
+                                    width={180}
+                                    height={180}
+                                    quality={90}
+                                    priority
+                                    placeholder="blur"
+                                    className="rounded-2xl object-cover min-w-[145px] w-1/3 h-[120px] md:w-[200px] md:h-[240px] xl:h-[180px]" />
 
                             </div>
-                            <div>
-                                <Image src={product.image} alt={product.label} width={485} height={400} quality={90} priority placeholder="blur" className="rounded-3xl object-cover" />
+                            <div className="md:w-fit xl:h-full">
+                                <Image
+                                    src={product.image}
+                                    alt={product.label}
+                                    width={485}
+                                    height={600}
+                                    quality={90}
+                                    priority
+                                    placeholder="blur"
+                                    className="rounded-3xl object-cover w-full h-[600px] md:w-[930px] md:h-[744px] xl:h-full xl:w-fit" />
                             </div>
                         </div>
-                        <div className="flex flex-col gap-6">
+                        <div className="flex flex-col gap-6 flex-1">
                             <div className="flex flex-col gap-2">
-                                <h1 className="text-[32px] text-black">{product.label}</h1>
-                                <p className="text-[#1f1f1f]">Upgrade your workspace with the eDisplay XS and take your productivity to new heights.</p>
+                                <h1 className="text-[32px] xl:text-[40px] text-black leading-tight w-fit">{product.label}</h1>
+                                <p className="text-sm md:text-base text-[#1f1f1f]">Upgrade your workspace with the eDisplay XS and take your productivity to new heights.</p>
                             </div>
-                            <h2 className="text-[32px] text-black w-fit">USD {product.price}</h2>
+                            <h2 className="text-2xl md:text-[28px] xl:text-[32px] text-black w-fit">USD {product.price}</h2>
                             <div className="flex flex-col w-full gap-4 items-center">
-                                <Link href={`/shop/${product.id}`} className="text-white bg-black text-center px-9 py-[18px] rounded-full w-full text-[18px]">
+                                <Link href={`/shop/${product.id}`} className="text-white bg-black text-center px-9 py-[18px] rounded-full w-full md:text-[18px]">
                                     Buy now
                                 </Link>
-                                <div className="flex flex-col gap-1 text-center text-[#7f7f7f] text-sm w-full">
+                                <div className="flex flex-col gap-1 text-center text-[#7f7f7f] text-[12px] md:text-sm w-full">
                                     <p>Estimate delivery times: 3-6 days (International)</p>
                                     <p>Return within 45 days of purchase. Duties & taxes are non-refundable.</p>
                                 </div>
                             </div>
-                            <div className="flex flex-col justify-end h-full">
+                            <div className="flex flex-col justify-end flex-1">
                                 <div className="flex flex-col gap-2">
                                     <div className="flex gap-2 justify-between py-4">
                                         <div className="flex gap-2 items-center text-black">
                                             <ShieldCheckIcon className="w-6 h-6" />
-                                            <p>Warranty</p>
+                                            <p className="text-sm md:text-base">Warranty</p>
                                         </div>
                                         <ArrowRightIcon className="w-6 h-6 text-black" />
                                     </div>
@@ -56,7 +88,7 @@ export default function ProductPage({ product }: { product: product }) {
                                     <div className="flex gap-2 items-center justify-between py-4">
                                         <div className="flex gap-2 items-center text-black">
                                             <Package className="w-6 h-6" />
-                                            <p>Shipping & delivery</p>
+                                            <p className="text-sm md:text-base">Shipping & delivery</p>
                                         </div>
                                         <ArrowRightIcon className="w-6 h-6 text-black" />
                                     </div>
@@ -64,7 +96,7 @@ export default function ProductPage({ product }: { product: product }) {
                                     <div className="flex gap-2 items-center justify-between py-4">
                                         <div className="flex gap-2 items-center text-black">
                                             <HeadsetIcon className="w-6 h-6" />
-                                            <p>Support</p>
+                                            <p className="text-sm md:text-base">Support</p>
                                         </div>
                                         <ArrowRightIcon className="w-6 h-6 text-black" />
                                     </div>
--- src/data/shopData.ts
+++ src/data/shopData.ts
@@ -17,20 +17,130 @@ import headphones_99 from "../../public/headphones-99.png"
 import headphoneGreen from "../../public/headphoneGreen.png"
 
 export const shopProducts: product[] = [
-    { id: 1, slug: 'edisplay-xg', label: 'eDisply XG', href: '/shop', image: displayXG, price: '649.99', },
-    { id: 2, slug: 'headphones-or-27n', label: 'Headphones or_27n', href: '/shop', image: orangeHeadphone, price: '97.99' },
-    { id: 3, slug: 'iphone-15-black', label: 'iphone_15 Black', href: '/shop', image: iphoneBlack, price: '799.99' },
-    { id: 4, slug: 'headphones-w-96c', label: 'Headphones w_96c', href: '/shop', image: headphone_96c, price: '129.99', },
-    { id: 5, slug: 'edisplay-xg', label: 'eDisplay XE', href: '/shop', image: displayXE, price: '649.99' },
-    { id: 6, slug: 'headphones-z-23c', label: 'Headphones z_23c', href: '/shop', image: headphoneBlack, price: '149.99' },
-    { id: 7, slug: 'iphone-15-red', label: 'iphone_15 Red', href: '/shop', image: iphoneRed, price: '799.99', },
-    { id: 8, slug: 'iphone-15-white', label: 'iphone_15 White', href: '/shop', image: iphoneWhite, price: '799.99' },
-    { id: 9, slug: 'ewatch-sf-3-black', label: 'eWatch SF_3 Black', href: '/shop', image: watchBlack, price: '319.99' },
-    { id: 10, slug: 'ewatch-sf-3-white', label: 'eWatch SF_3 White', href: '/shop', image: watchWhite, price: '319.99' },
-    { id: 11, slug: 'edisplay-xs', label: 'eDisplay XS', href: '/shop', image: displayXS, price: '649.99' },
-    { id: 12, slug: 'headphone-w-99', label: 'Headphone w_99', href: '/shop', image: headphones_99, price: '129.99' },
-    { id: 13, slug: 'headphone-e-11g', label: 'Headphone e_11g', href: '/shop', image: headphoneGreen, price: '97.99' },
-    { id: 14, slug: 'headphones-x-28m', label: 'Headphones x_28m', href: '/shop', image: headphone, price: '149.99', },
-    { id: 15, slug: 'ewatch-sf-4', label: 'eWatch SF_4', href: '/shop', image: eWatch, price: '349.99', },
-    { id: 16, slug: 'edisplay-xr', label: 'eDisplay XR', href: '/shop', image: eDisplay, price: '649.99', },
+    {
+        id: 1,
+        slug: 'edisplay-xg',
+        label: 'eDisplay XG',
+        href: '/shop',
+        image: displayXG,
+        price: '649.99'
+    },
+    {
+        id: 2,
+        slug: 'headphones-or-27n',
+        label: 'Headphones or-27n',
+        href: '/shop',
+        image: orangeHeadphone,
+        price: '97.99'
+    },
+    {
+        id: 3,
+        slug: 'iphone-15-black',
+        label: 'iphone 15 Black',
+        href: '/shop',
+        image: iphoneBlack,
+        price: '799.99'
+    },
+    {
+        id: 4,
+        slug: 'headphones-w-96c',
+        label: 'Headphones w-96c',
+        href: '/shop',
+        image: headphone_96c,
+        price: '129.99'
+    },
+    {
+        id: 5,
+        slug: 'edisplay-xg',
+        label: 'eDisplay XE',
+        href: '/shop',
+        image: displayXE,
+        price: '649.99'
+    },
+    {
+        id: 6,
+        slug: 'headphones-z-23c',
+        label: 'Headphones z-23c',
+        href: '/shop',
+        image: headphoneBlack,
+        price: '149.99'
+    },
+    {
+        id: 7,
+        slug: 'iphone-15-red',
+        label: 'iphone_15 Red',
+        href: '/shop',
+        image: iphoneRed,
+        price: '799.99'
+    },
+    {
+        id: 8,
+        slug: 'iphone-15-white',
+        label: 'iphone 15 White',
+        href: '/shop',
+        image: iphoneWhite,
+        price: '799.99'
+    },
+    {
+        id: 9,
+        slug: 'ewatch-sf-3-black',
+        label: 'eWatch SF 3 Black',
+        href: '/shop',
+        image: watchBlack,
+        price: '319.99'
+    },
+    {
+        id: 10,
+        slug: 'ewatch-sf-3-white',
+        label: 'eWatch SF 3 White',
+        href: '/shop',
+        image: watchWhite,
+        price: '319.99'
+    },
+    {
+        id: 11,
+        slug: 'edisplay-xs',
+        label: 'eDisplay XS', href: '/shop',
+        image: displayXS,
+        price: '649.99'
+    },
+    {
+        id: 12,
+        slug: 'headphone-w-99',
+        label: 'Headphone w-99', href: '/shop',
+        image: headphones_99,
+        price: '129.99'
+    },
+    {
+        id: 13,
+        slug: 'headphone-e-11g',
+        label: 'Headphone e-11g',
+        href: '/shop',
+        image: headphoneGreen,
+        price: '97.99'
+    },
+    {
+        id: 14,
+        slug: 'headphones-x-28m',
+        label: 'Headphones x-28m',
+        href: '/shop',
+        image: headphone,
+        price: '149.99',
+    },
+    {
+        id: 15,
+        slug: 'ewatch-sf-4',
+        label: 'eWatch SF-4',
+        href: '/shop',
+        image: eWatch,
+        price: '349.99',
+    },
+    {
+        id: 16,
+        slug: 'edisplay-xr',
+        label: 'eDisplay XR',
+        href: '/shop',
+        image: eDisplay,
+        price: '649.99',
+    },
 ]
